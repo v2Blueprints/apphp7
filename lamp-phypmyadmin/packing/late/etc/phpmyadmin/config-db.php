2ee9cbf32e1f352129bd39888ce45e97a6e210ed
James Vodanovich <james@jvodan.com>
2021-01-04 11:02:27 +1100

    init lamp-phpmyadmin

---
+++ lamp-phypmyadmin/packing/late/etc/phpmyadmin/config-db.php
@@ -0,0 +1,10 @@
+<?php
+##
+
+$dbuser='^^mariadb.username^^';
+$dbpass='^^mariadb.password^^';
+$basepath='';
+$dbname='^^mariadb.database_name^^';
+$dbserver='^^mariadb.hostname^^';
+$dbport='3306';
+$dbtype='mariadb';
